920f6f3fefc8fffc661e42c9dce5fcb06e3c073d
MoatasemAshmawy <thedashash9@gmail.com>
2023-12-22 21:43:19 +0200

    [backend] feat: implement extraction api

--- server/main.py
+++ server/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+import os
+import uuid
+import cv2
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import numpy as np
+from signature_extractor import extract_signature
 
 app = FastAPI()
 
@@ -11,10 +17,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/images", StaticFiles(directory="./outputs"), name="outputs")
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/api/data")
-def get_data():
+@app.post("/api/match")
+def match(image1: UploadFile, image2: UploadFile):
     return {"example_data": [1, 2, 3, 4, 5]}
+
+@app.post("/api/extract")
+async def extract(image: UploadFile):
+    if not image:
+        return {"message": "No upload file sent"}
+    else:
+        if image.content_type != "image/png" and image.content_type != "image/jpeg":
+            raise HTTPException(status_code=415,detail="Only Png and Jpeg are supported.")
+    input_file_path = "./inputs/input_" + str(uuid.uuid4()) +".png"
+    with open(input_file_path, "+wb") as file:
+        content = await image.read()
+        file.write(content)
+    output_file_name = extract_signature(input_file_path)
+    os.remove(input_file_path)
+    return {"image_path": f"images/{output_file_name}"}
+    
--- server/signature_extractor.py
+++ server/signature_extractor.py
@@ -12,6 +12,7 @@ from skimage.color import label2rgb
 from skimage.measure import regionprops
 import numpy as np
 import uuid
+import os
 
 def extract_signature(image_path):
     # the parameters are used to remove small size connected pixels outliar 
@@ -28,18 +29,20 @@ def extract_signature(image_path):
 
     # connected component analysis by scikit-learn framework
     blobs = img > img.mean()
+    print(blobs)
+
     blobs_labels = measure.label(blobs, background=1)
-    image_label_overlay = label2rgb(blobs_labels, image=img)
+    # image_label_overlay = label2rgb(blobs_labels, image=img)
 
-    fig, ax = plt.subplots(figsize=(10, 6))
+    # fig, ax = plt.subplots(figsize=(10, 6))
 
-    '''
-    # plot the connected components (for debugging)
-    ax.imshow(image_label_overlay)
-    ax.set_axis_off()
-    plt.tight_layout()
-    plt.show()
-    '''
+ 
+    # # plot the connected components (for debugging)
+    # ax.imshow(image_label_overlay)
+    # ax.set_axis_off()
+    # plt.tight_layout()
+    # plt.show()
+   
 
     the_biggest_component = 0
     total_area = 0
@@ -56,20 +59,20 @@ def extract_signature(image_path):
                 the_biggest_component = region.area
 
     average = (total_area/counter)
-    print("the_biggest_component: " + str(the_biggest_component))
-    print("average: " + str(average))
+    # print("the_biggest_component: " + str(the_biggest_component))
+    # print("average: " + str(average))
 
     # experimental-based ratio calculation, modify it for your cases
     # a4_small_size_outliar_constant is used as a threshold value to remove connected outliar connected pixels
     # are smaller than a4_small_size_outliar_constant for A4 size scanned documents
     a4_small_size_outliar_constant = ((average/constant_parameter_1)*constant_parameter_2)+constant_parameter_3
-    print("a4_small_size_outliar_constant: " + str(a4_small_size_outliar_constant))
+    # print("a4_small_size_outliar_constant: " + str(a4_small_size_outliar_constant))
 
     # experimental-based ratio calculation, modify it for your cases
     # a4_big_size_outliar_constant is used as a threshold value to remove outliar connected pixels
     # are bigger than a4_big_size_outliar_constant for A4 size scanned documents
     a4_big_size_outliar_constant = a4_small_size_outliar_constant*constant_parameter_4
-    print("a4_big_size_outliar_constant: " + str(a4_big_size_outliar_constant))
+    # print("a4_big_size_outliar_constant: " + str(a4_big_size_outliar_constant))
 
     # remove the connected pixels are smaller than a4_small_size_outliar_constant
     pre_version = morphology.remove_small_objects(blobs_labels, a4_small_size_outliar_constant)
@@ -81,14 +84,20 @@ def extract_signature(image_path):
     pre_version[too_small_mask] = 0
     # save the the pre-version which is the image is labelled with colors
     # as considering connected components
-    plt.imsave('pre_version.png', pre_version)
+    pre_version_path = "./outputs/pre_version_" + str(uuid.uuid4()) + ".png"
+    cv2.imwrite(pre_version_path, pre_version)
 
     # read the pre-version
-    img = cv2.imread('pre_version.png', 0)
+    img = cv2.imread(pre_version_path, 0)
     # ensure binary
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     # save the the result
-    output_path = "output_"+ str(uuid.uuid4())+".png"
-    cv2.imwrite(f"./outputs/{output_path}", img)
+    output_file_name = "output_"+ str(uuid.uuid4())+".png"
+    cv2.imwrite(f"./outputs/{output_file_name}", img)
+    os.remove(pre_version_path)
 
-    return output_path
+    return output_file_name
+
+
+if __name__ == "__main__":
+   extract_signature("./inputs/in1.jpg")
