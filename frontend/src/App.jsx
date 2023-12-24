cea5be0b1e94a35011bdbcbf9052fd288c1b8f6d
MoatasemAshmawy <thedashash9@gmail.com>
2023-12-25 01:15:58 +0200

    [frontend] fix: add base url in router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,18 @@ import Root from './Root'
 import SignatureExtraction from './SignatureExtraction'
 import SignatureMatching from './SignatureMatching'
 import Hero from './Hero'
-
-function App() {
  
 
+function App() {
+ const baseUrl = import.meta.env.BASE_URL;
+
   return (
-    <Router>
+    <Router basename={baseUrl}>
       <Routes>
         <Route path='/' element={<Root/>}>
           <Route path ='/' element={<Hero/>}/>
-          <Route path='extraction' element={<SignatureExtraction/>} />
-          <Route path='matcher' element={<SignatureMatching/>} />
+          <Route path='/extraction' element={<SignatureExtraction/>} />
+          <Route path='/matcher' element={<SignatureMatching/>} />
         </Route>
       </Routes>
     </Router>
